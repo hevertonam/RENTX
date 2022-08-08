1fe62660a15741bffdf4eb16c1a9ce9390b004cc
Heverton <hevertonam2018@gmail.com>
2022-08-08 19:04:18 -0300

    configurando swagger

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express, { request, response } from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "./swagger.json";
+import { router } from './routes';
 
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/modelo",categoriesRoutes); 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use("/modelo",specificationsRoutes); 
+app.use(router);
+
+
 
 app.listen(3333, () => console.log("Server is running ")); 
