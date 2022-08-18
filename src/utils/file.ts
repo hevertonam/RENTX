c76b2b7dcb6ecdcc1508e95719d15505c65bf78d
Heverton <hevertonam2018@gmail.com>
2022-08-18 18:08:13 -0300

    remover arquivo de avatar existente

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,5 +1,13 @@
 import fs from "fs";
 
-export const deleteFile = async(filenam: string) =>{
+export const deleteFile = async(filename: string) =>{
 
+try {
+
+    await fs.promises.stat(filename);
+} catch  {
+    return;
+    
 }
+await fs.promises.unlink(filename);
+};
