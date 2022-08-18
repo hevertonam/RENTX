ae52534fa23e757f4af43de00c8c517d68879131
Heverton <hevertonam2018@gmail.com>
2022-08-18 17:56:09 -0300

    upload do avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,14 @@
 import {Router} from "express";
 import multer from "multer";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 import { CreateUserController } from "../modules/accounts/entities/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/entities/useCases/createUser/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
-const upload = multer({
-    dest: "./avatar"
-})
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 
@@ -15,6 +16,6 @@ const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
 
-usersRoutes.patch("/avatar", upload.single("file"), updateUserAvatarController.handle);
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle);
 
 export {usersRoutes};
