8353ab7887d49996bad17f791166621762a9cf52
Heverton <hevertonam2018@gmail.com>
2022-08-19 12:13:16 -0300

    Criando Testes para criação de categoria

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,17 +1,59 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./ CreateCategoryUseCase"
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () =>{
+
+beforeEach(() =>{
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+})
+
+it("should be able to create a new category", async () =>{
+    const category = {
+        nome: "Category Test",
+        descricao: "Category description test",
+    }
+
+   await createCategoryUseCase.execute({
+        nome: category.nome,
+        descricao: category.descricao,
+    });
+    
+    const categoryCreated = await  categoriesRepositoryInMemory.findByName(
+        category.nome
+    );
 
 
-describe("Criar categoria",()=> {
-    it("Espero que 2 + 2 seja 4", () => {
-        const soma = 2 + 2 ;
-        const resultado = 5;
 
-        expect(soma).toBe(resultado);
+    expect(categoryCreated).toHaveProperty("id");
+});
+ 
+
+it("should not be able to create a new category with name exists", async () =>{ 
+
+    expect(async () =>{
+
+    const category = {
+        nome: "Category Test",
+        descricao: "Category description test",
+    }
+
+   await createCategoryUseCase.execute({
+        nome: category.nome,
+        descricao: category.descricao,
     });
 
-    it("Espero que 2 + 2 não seja 5", () =>{
-        const soma = 2 + 2;
-        const resultado = 5;
+    await createCategoryUseCase.execute({
+        nome: category.nome,
+        descricao: category.descricao,
+    });
+    }).rejects.toBeInstanceOf(AppError);
+    
 
-        expect(soma).not.toBe(resultado);
-    })
 });
+
+})
